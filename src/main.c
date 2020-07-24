ef39d91881a4be756e2e631c07049866192029be
ryderstanley <ryderstanley2007@gmail.com>
2020-07-24 14:03:07 -0600

    created main file

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char[100] q1 = "hello";
+char[100] q2 = "what are you doing";
+char[100] q3 = "how are you";
+char[100] q4 = "are you a bot";
+char[100] q5 = "what day is it";
